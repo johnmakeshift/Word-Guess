a71cdaa5b95deb268d693c414f909075920fda32
John Wilson <gabrielcrowe@gmail.com>
2019-07-06 09:47:22 -0700

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,28 @@ var wordSelection = ["Car1","Car2",
 "Dog1","Dog2",
 "cat1","cat2",
 "color1","color2",
-"shape1","shape2"];
+"shape1","shape2","borrow",
+"motif",
+'entry',
+"organize",
+"ground",
+"Member",
+"limit",
+"quarter",
+"hot",
+"archive",
+"quotation",
+"remain",
+"temporary",
+"fun",
+"absolute",
+"fault",
+"white",
+"marble",
+"Ranch",
+"deliver",
+"shorts",
+"bishop"];
 
 
 
@@ -20,7 +41,10 @@ var arrayLength = wordSelection.length;
 // var wordLength = wordChoice.length
 //creates a var for the random numner that is maxed out for the array length var...works
 var randomWord = wordSelection[Math.floor(Math.random()*arrayLength)];
-// console.log(arrayLength);
-// console.log(randomWord);
+//getting the character length of the selected word
 var wordLength = randomWord.length;
+var hiddenLetters =("_");
+console.log(arrayLength);
+console.log(randomWord);
 console.log(wordLength);
+// console.log(hiddenLetters*wordLength);
